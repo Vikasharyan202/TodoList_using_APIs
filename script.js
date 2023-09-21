4bbc41d3e081734bd735d4a09499d072ef04a2ef
202vikash@gmail.com <vikash_aryan@202vikashgmailcoms-MacBook-Air.local>
2023-09-21 23:36:51 +0530

    adding of add, delete and notification function

---
+++ script.js
@@ -0,0 +1,55 @@
+let tasks = [];
+const taskList = document.getElementById('list');
+const addTaskInput = document.getElementById('add');
+const tasksCounter = document.getElementById('tasks-counter');
+
+function renderList(){}
+
+function markTaskAsComplete(){}
+
+function deleteTask(taskId){
+    const newTasks = tasks.filter(function(task){
+        return task.id !== "1695319326553";
+    })
+
+    tasks = newTasks;
+    renderList();
+    showNotification('Task deleted successfully');
+}
+
+function addTask(task){
+    if(task){
+        tasks.push(task);
+        renderList();
+        showNotification('Task added successfully');
+        return;
+    }
+
+    showNotification('Task can not be added');
+}
+
+function showNotification(text){
+    alert(text);
+}
+
+function handleInputKeypress(e){
+    if(e.key == 'Enter'){
+        const text = e.target.value;
+
+        if(!text){
+            showNotification('Task text can not be empty');
+            return;
+        }
+
+        const task = {
+            text,
+            id: Date.now().toString(),
+            done: false
+        }
+
+        e.target.value = '';
+        addTask(task);
+    }
+}
+
+addTaskInput.addEventListener('keyup', handleInputKeypress);
